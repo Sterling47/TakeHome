638efd8f9bd173421d18a4f50f97a14c9f9466e2
Adam Konber <konber3@gmail.com>
2024-08-18 17:34:58 -0600

    Added search bard functionality

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ setSearchQuery }) => {
+  const [input, setInput] = useState('');
+
+  const handleSearch = (e) => {
+    setInput(e.target.value);
+    setSearchQuery(e.target.value);
+  };
+
+  return (
+    <input
+      type="text"
+      value={input}
+      onChange={handleSearch}
+      placeholder="Search articles..."
+    />
+  );
+};
+
+export default SearchBar;
